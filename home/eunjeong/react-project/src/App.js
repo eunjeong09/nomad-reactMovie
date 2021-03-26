62ee38d9b3ae3e29839ffea7f285295c41540e01
eunjeong09 <ejko0911@gmail.com>
2021-03-27 03:10:00 +0900

    Add router, detail component 리액트 라우터를 사용해서 디테일 페이지 구현

--- home/eunjeong/react-project/src/App.js
+++ home/eunjeong/react-project/src/App.js
@@ -1,21 +1,19 @@
 import React from "react";
-import axios from "axios";
+import { HashRouter, Route } from "react-router-dom";
+import Home from "./routes/Home";
+import About from "./routes/About";
+import Detail from "./routes/Detail";
+import Navigation from "./components/Navigation";
 
-class App extends React.Component {
-  state = {
-    isLoading: true,
-    movies: [],
-  };
-  getMovies = async () => {
-    const {data : {data :{movies}}} = await axios.get("https://yts-proxy.now.sh/list_movies.json");
-    console.log({movies, isLoading:false});
-  }
-  componentDidMount() {
-    this.getMovies();
-  }
-  render() {
-    const { isLoading } = this.state;
-    return <div>{isLoading ? "Loading..." : "We are ready"}</div>;
-  }
+function App() {
+  return (
+    <HashRouter>
+      <Navigation />
+      <Route path="/" exact={true} component={Home} />
+      <Route path="/about" component={About} />
+      <Route path="/movie/:id" component={Detail} />
+    </HashRouter>
+  );
 }
+
 export default App;
